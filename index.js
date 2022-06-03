215a5012e0ed1ef374b5305200ae3be0743694e5
Alvaro125 <alvaromartinezferreira@gmail.com>
2022-06-03 09:34:42 -0300

    first
    first

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("button1");
+
+button.onclick = (e)=>{
+    console.log("Hello World!")
+}
+// function test(){
+//     console.log("Hello World!")
+// }
+// button.addEventListener('click',test, true)
